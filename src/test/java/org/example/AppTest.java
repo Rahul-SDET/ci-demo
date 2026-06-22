a4a6ed97a1cb2aac69f963431381e7e25bf984ca
Rahul Patil <rahulpatil@Rahuls-MacBook-Pro.local>
2026-06-22 14:23:59 +0530

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void additionWorks() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+
+    @Test
+    void failingTest() {
+        int result = 2 + 2;
+        assertEquals(5, result); // this will fail on purpose
+    }
+}
